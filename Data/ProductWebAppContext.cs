2296af636f85eb618f9f0e00086388d657c0bd5b
DivyShikha <divyshikha1890@gmail.com>
2025-08-20 10:17:44 +0530

    made changes  appsetting.json

--- Data/ProductWebAppContext.cs
+++ Data/ProductWebAppContext.cs
@@ -13,19 +13,19 @@ namespace ProductWebApp.Data
 
         // Hardcoded connection string (matches your server & DB)
         // Tip: for production, prefer appsettings.json + DI.
-        static string connectionString =
-            @"Data Source=IBM-FNZ3SB4\SQLEXPRESS;Initial Catalog=ProductDB;TrustServerCertificate=True;Integrated Security=True;";
+       // static string connectionString =
+           // @"Data Source=IBM-FNZ3SB4\SQLEXPRESS;Initial Catalog=ProductDB;TrustServerCertificate=True;Integrated Security=True;";
 
         public DbSet<Product> Product { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // If you want DI to win when provided, uncomment the guard:
-            // if (!optionsBuilder.IsConfigured)
-            // {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(connectionString);
-            // }
+            if (!optionsBuilder.IsConfigured)
+            {
+                base.OnConfiguring(optionsBuilder);
+            //optionsBuilder.UseSqlServer(connectionString);
+            }
         }
 
         // Optional: map explicitly to the "Product" table
